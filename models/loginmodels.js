f95efbd53166f583264aebad9502acda075720ea
suhaila hawsawi <suhila0987@outlook.com>
2024-09-25 20:43:57 +0300

    create Account update / fix issue with routring

---
+++ models/loginmodels.js
@@ -0,0 +1,13 @@
+/*const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  idNumber: String,
+  phoneNumber: String,
+  email: String,
+  password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;*/
