38f22fc7c844e78a1050007c70fc00ac5d6b74d9
suhaila hawsawi <suhila0987@outlook.com>
2024-10-29 22:53:29 +0300

    delete unused code

--- public/js/recommendations.js
+++ public/js/recommendations.js
@@ -66,34 +66,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
-/*
-const submitButton = document.querySelector('.btn-submit1');
-const errorMessage1 = document.getElementById('error-message1');
-const form1 = document.getElementById('recommendation-form');
-
-function validateForm1() {
-    const fields = form1.querySelectorAll('select');
-    let isValid = true;
-    fields.forEach(field => {
-        if (field.value === "") {
-            field.style.border = "2px solid red";
-            isValid = false;
-        } else {
-            field.style.border = "";
-        }
-    });
-    console.log('Validation result:', isValid); // Log validation result
-    return isValid;
-}
-
-submitButton.addEventListener('click', (event) => {
-    errorMessage1.style.display = 'none';
-    console.log('Submit button clicked'); // Log button click
-    if (!validateForm1()) {
-        event.preventDefault();
-        errorMessage1.textContent = 'لا تترك حقول فارغة';
-        errorMessage1.style.display = 'block';
-    }
-});
-*/
